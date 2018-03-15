0475de4dd19ecedf951977583acc8076c5412c75
Dautrey94 <Dautrey94@att.net>
2018-03-15 09:53:51 -0400

    Add Signup Component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signUpInfo = {
+    username: '',
+    password: ''
+  };
 
-  constructor() { }
+  errorMessage: String;
+
+  constructor(private myAuth: AuthService, private myRouter: Router) { }
 
   ngOnInit() {
+  }
+
+  doSignUp(){
+    this.myAuth
+      .signup(this.signUpInfo)
+      .then(resultFromApi => {
+        this.signUpInfo = { username: '', password: ''};
+
+        this.errorMessage = '';
+
+        this.myRouter.navigate([/travelcards]);
+      })
+
+      .catch(err => {
+        const parsedError = err.json();
+        this.errorMessage = parsedError.message + 'error'
+      });
   }
 
 }
